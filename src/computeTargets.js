bce4655da9d3ce72b5d692b2b400775e22dad663
Dmitry Sharabin <dmitrysharabin@gmail.com>
2024-08-27 23:47:25 +0200

    Targeting links should have the `page-number` class

--- src/computeTargets.js
+++ src/computeTargets.js
@@ -3,7 +3,7 @@ import * as util from "./util.js";
 export default function computeTargets (root = document.documentElement) {
 	let info = {};
 	let start = performance.now();
-	let links = root.querySelectorAll("a[href^='#']:not(.page-number)");
+	let links = root.querySelectorAll("a[href^='#'].page-number");
 
 	for (let a of links) {
 		// Why not querySelector? So we don’t get errors for invalid selectors
