89c193dfc4ea2ad35bb7f1b7f10f0a2a9d1271c9
Lea Verou <lea@verou.me>
2024-09-05 14:45:17 +0300

    NodeStack: Make it extend Array directly and drop private variables

--- src/util/NodeStack.js
+++ src/util/NodeStack.js
@@ -1,46 +1,63 @@
-export default class NodeStack {
-	#all;
-	#weak;
+/**
+ * A class to manage stacks of nodes for pagination managing both strongly and weakly held nodes.
+ * Weakly held nodes that are not followed by a strongly held node are removed at the end.
+ * Not all array methods are implemented, avoid modifying the array with anything other than push, pop, and splice
+ */
+export default class NodeStack extends Array {
 	#lengthStrong;
 
 	constructor (container) {
+		super();
 		this.container = container;
 		this.range = container.ownerDocument.createRange();
 		this.range.setStart(container, 0);
 		this.range.setEnd(container, 0);
-		this.#all = [];
 		this.#lengthStrong = 0;
 
 		// these shouldn't trigger anything by themselves, but should be taken along for the ride if a node after them gets moved
 		// and should never be the last node in the stack in the final state
 		// This is just an array of booleans describing the values in #all
-		this.#weak = [];
+		this.weak = [];
 	}
 
 	get last () {
-		return this.#all.at(-1);
+		return this.at(-1);
 	}
 
 	get lastStrong () {
-		return this.#all.at(-1 - this.#weak.slice().reverse().indexOf(false));
-	}
-
-	get length () {
-		return this.#all.length;
+		return this.at(-1 - this.weak.slice().reverse().indexOf(false));
 	}
 
 	get lengthStrong () {
 		return this.#lengthStrong;
 	}
 
-	[Symbol.iterator] () {
-		return this.#all[Symbol.iterator]();
-	}
-
-	#heights = [];
+	heights = [];
 
 	get height () {
-		return this.#heights[this.#heights.length - 1] ??= this.range.getBoundingClientRect().height;
+		if (this.length === 0) {
+			return 0;
+		}
+
+		return this.heightAt(-1);
+	}
+
+	heightAt (nodeOrIndex) {
+		let index = nodeOrIndex > 0 ? nodeOrIndex : nodeOrIndex < 0 ? this.length + nodeOrIndex : this.indexOf(nodeOrIndex);
+		let node = this[index];
+
+		if (!node) {
+			throw new Error("Node not in stack:", nodeOrIndex);
+		}
+
+		if (this.heights[index] === undefined) {
+			// We need to calculate it
+			this.range.setEndAfter(node);
+			this.heights[index] = this.range.getBoundingClientRect().height;
+			this.#update();
+		}
+
+		return this.heights[index];
 	}
 
 	#update () {
@@ -53,18 +70,18 @@ export default class NodeStack {
 	}
 
 	pushWeak (...nodes) {
-		this.#weak.push(...nodes.map(_ => true));
-		this.#heights.push(...nodes.map(_ => undefined));
-		let ret = this.#all.push(...nodes);
+		this.weak.push(...nodes.map(_ => true));
+		this.heights.push(...nodes.map(_ => undefined));
+		let ret = super.push(...nodes);
 		this.#update();
 		return ret;
 	}
 
 	push (...nodes) {
 		this.#lengthStrong += nodes.length;
-		this.#weak.push(...nodes.map(_ => false));
-		this.#heights.push(...nodes.map(_ => undefined));
-		let ret = this.#all.push(...nodes);
+		this.weak.push(...nodes.map(_ => false));
+		this.heights.push(...nodes.map(_ => undefined));
+		let ret = super.push(...nodes);
 		this.#update();
 		return ret;
 	}
@@ -72,10 +89,10 @@ export default class NodeStack {
 	popWeak () {
 		let ret = [];
 
-		while (this.#weak.at(-1)) {
-			this.#weak.pop();
-			this.#all.pop();
-			this.#heights.pop();
+		while (this.length > 0 && this.weak.at(-1)) {
+			this.weak.pop();
+			ret.push(this.pop());
+			this.heights.pop();
 		}
 
 		this.#update();
@@ -84,9 +101,10 @@ export default class NodeStack {
 	}
 
 	pop () {
-		let ret = this.#all.pop();
-		let wasWeak = this.#weak.pop();
-		this.#heights.pop();
+		let ret = super.pop();
+		let wasWeak = this.weak.pop();
+		this.heights.pop();
+
 		if (!wasWeak) {
 			this.#lengthStrong--;
 		}
