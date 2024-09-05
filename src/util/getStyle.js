879a44d9da2560fbbaec424627f2cf271d2b9464
Lea Verou <lea@verou.me>
2024-09-05 18:29:59 +0300

    [util] Move getStyle to separate module

---
+++ src/util/getStyle.js
@@ -0,0 +1,35 @@
+const styles = new WeakMap();
+const categoricalProperties = ["position", "display", "box-sizing", "page-break-before", "page-break-inside", "page-break-after", "text-wrap"];
+const numericalProperties = ["padding-block-start", "padding-block-end", "min-height", "line-height"];
+
+export default function getStyle (node) {
+	if (!node || node instanceof Node && node.nodeType !== 1) {
+		return null;
+	}
+
+	let style = styles.get(node);
+
+	if (style) {
+		return style;
+	}
+
+	style = {};
+	let cs = node instanceof CSSStyleDeclaration ? node : getComputedStyle(node);
+
+	for (let cssProperty of categoricalProperties) {
+		let property = cssProperty.replace(/^page-/, "").replaceAll("-", "_");
+		let value = cs.getPropertyValue(cssProperty);
+
+		if (value !== "auto") {
+			style[property] = value;
+		}
+	}
+
+	for (let cssProperty of numericalProperties) {
+		let property = cssProperty.replaceAll("-", "_");
+		style[property] = parseFloat(cs.getPropertyValue(cssProperty)) || 0;
+	}
+
+	styles.set(node, style);
+	return style;
+}
